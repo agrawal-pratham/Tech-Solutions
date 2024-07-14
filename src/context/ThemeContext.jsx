72a3b095925afee9efe36506bf465d3007e93651
Pratham <agrawalpratham2002@gmail.com>
2024-07-15 00:07:11 +0530

    added existing theme preference

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -7,6 +7,18 @@ const ThemeContext = createContext();
 export const ThemeContextProvider = ({ children }) => {
   const [darkMode, setDarkMode] = useState(false);
 
+  const handleThemeInit = () => {
+    if (
+      localStorage.getItem("theme") === Theme.Dark ||
+      (window.matchMedia &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      localStorage.clear();
+      document.documentElement.classList.add(Theme.Dark);
+      setDarkMode(true);
+    }
+  };
+
   const handleChangeTheme = () => {
     if (!darkMode) {
       document.documentElement.classList.add(Theme.Dark);
@@ -20,7 +32,7 @@ export const ThemeContextProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    handleChangeTheme();
+    handleThemeInit();
   }, []);
 
   return (
