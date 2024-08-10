475b05c90ca6d208fe61f41cc0b20637c4fc3243
Pratham <agrawalpratham2002@gmail.com>
2024-08-10 22:27:51 +0530

    services

--- src/pages/services.js
+++ src/pages/services.js
@@ -2,7 +2,7 @@ export default function services() {
   return (
     <div className="flex justify-center items-center h-screen">
       <p className="text-center text-5xl text-gray-600 dark:text-slate-300">
-        This site is currently under development
+        Services
       </p>
     </div>
   );
